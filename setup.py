e94a57902e2a3d0d44c20b04a2804263f428426a
Tobias Rosskopf <42942019+TobiasRosskopf@users.noreply.github.com>
2019-11-27 17:28:18 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,47 @@
+#! python3.8
+# -*- coding: utf-8 -*-
+
+# File name:    setup.py
+# Author:       Tobias Rosskopf
+# Email:        tobirosskopf@gmail.com
+# Created:      ??.??.2019
+# Modified:     ??.??.2019
+
+
+"""
+Setup docstring
+    USE:
+    $ python setup.py register
+    $ python setup.py upload
+"""
+
+# Standard imports
+from setuptools import setup
+
+# Constants
+PACKAGE_NAME = 'package_name'
+PACKAGE_LIST = ['package_name',]
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    # Metadata
+    name = PACKAGE_NAME,
+    version = '1.0',
+    description = 'Write description!',
+    long_description = long_description,
+    license = 'MIT',
+    author = 'Tobias Rosskopf',
+    author_email = 'tobirosskopf@gmail.com',
+    url = '',
+
+    # Package info
+    python_requires = '>=3.8',
+    packages = [PACKAGE_NAME] + PACKAGE_LIST,
+    install_requires = requirements,
+    keywords = '',
+)
